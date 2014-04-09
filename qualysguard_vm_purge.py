2c2e131cd4033182c2eb7671319d66f53538d297
Parag Baxi <ghee22@gmail.com>
2014-04-09 12:56:51 -0400

    Update qualysguard_vm_purge.py

--- qualysguard_vm_purge.py
+++ qualysguard_vm_purge.py
@@ -5,7 +5,7 @@ qgc = qualysapi.connect()
 # Capture number of days from command parameter.
 number_of_days = sys.argv[1]
 # Request assets older than number_of_days.
-assets = qgc.request('asset_search.php', {'last_scan': 'not_within:164', 'target_asset_groups': 'All'})
+assets = qgc.request('asset_search.php', {'last_scan': 'not_within:%s' % number_of_days, 'target_asset_groups': 'All'})
 root = objectify.fromstring(assets)
 # Parse assets XML for list of IPs.
 ips = ''
